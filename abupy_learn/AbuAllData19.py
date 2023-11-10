70ecde771c19a3eb4acdd6d4cf2e9aa9346bc854
cwyspy100 <cwyspy100@163.com>
2023-11-10 17:05:26 +0800

    prac pandas

--- abupy_learn/AbuAllData19.py
+++ abupy_learn/AbuAllData19.py
@@ -5,9 +5,12 @@ from abupy import EMarketSourceType, EDataCacheType, EMarketTargetType, EMarketD
 from abupy import abu, ABuSymbolPd
 
 if __name__ == '__main__':
-    abupy.env.g_market_source = EMarketSourceType.E_MARKET_SOURCE_tx
-    abupy.env.g_data_cache_type = EDataCacheType.E_DATA_CACHE_CSV
-    # abu.run_kl_update(start='2020-07-01', end='2021-07-01', market=EMarketTargetType.E_MARKET_TARGET_CN, n_jobs=10)
-    abupy.env.g_data_fetch_mode = EMarketDataFetchMode.E_DATA_FETCH_FORCE_LOCAL
-    print(ABuSymbolPd.make_kl_df('sh000003').tail())
-    print(abupy.env.g_project_data_dir)
+    # abupy.env.g_market_source = EMarketSourceType.E_MARKET_SOURCE_sn_us
+    # abupy.env.g_data_cache_type = EDataCacheType.E_DATA_CACHE_CSV
+    # # abu.run_kl_update(start='2020-07-01', end='2021-07-01', market=EMarketTargetType.E_MARKET_TARGET_CN, n_jobs=10)
+    # abupy.env.g_data_fetch_mode = EMarketDataFetchMode.E_DATA_FETCH_FORCE_LOCAL
+    # print(ABuSymbolPd.make_kl_df('usTSLA').tail())
+    # print(abupy.env.g_project_data_dir)
+
+    abupy.env.disable_example_env_ipython()
+    print(ABuSymbolPd.make_kl_df('hk00213').tail())
