51aa777a620c323c862a5fee61d7c7b193ceb1ed
cwyspy100 <cwyspy100@163.com>
2024-03-08 15:11:46 +0800

    add cy_quant

---
+++ cy_quant/test_TradeVolumeStrategy.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import unittest
+from TradeVolumeStrategy import TradeVolumeStrategy
+import abupy 
+
+
+class TestTradeVolumeStrategy(unittest.TestCase):
+    def setUp(self):
+        self.strategy = TradeVolumeStrategy(day_num=5)
+
+    def test_fit_pick(self):
+        kl_pd = pd.DataFrame({'volume': [100, 200, 300, 400, 500, 600]})
+        target_symbol = 'AAPL'
+        result = self.strategy.fit_pick(kl_pd, target_symbol)
+        self.assertTrue(result)
+
+        kl_pd = pd.DataFrame({'volume': [100, 200, 300, 400, 500, 100]})
+        target_symbol = 'AAPL'
+        result = self.strategy.fit_pick(kl_pd, target_symbol)
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
