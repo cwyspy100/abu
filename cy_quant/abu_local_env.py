e071c1dac78da4de73d8d74041109265edcd94db
cwyspy100 <cwyspy100@163.com>
2024-03-13 21:52:02 +0800

    提交执行逻辑

---
+++ cy_quant/abu_local_env.py
@@ -0,0 +1,15 @@
+# -*- encoding:utf-8 -*-
+import os
+import sys
+
+sys.path.append(os.path.abspath('../'))
+os.system('')
+
+# print(sys.version)
+# 打印当前环境变量路径
+# print(os.environ['PATH'])
+
+# sys.path.append(os.path.abspath('../abupy'))
+
+# print(sys.path)
+# Now you can directly call functions from the abupy module
