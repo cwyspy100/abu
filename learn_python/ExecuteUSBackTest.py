f7a4491cb707481acab18a860f71d1e40ae2b6b2
cwyspy100 <cwyspy100@163.com>
2024-03-15 18:57:37 +0800

    test ma buy

--- learn_python/ExecuteUSBackTest.py
+++ learn_python/ExecuteUSBackTest.py
@@ -23,6 +23,7 @@ from abupy import AbuFactorCloseAtrNStop, AbuFactorAtrNStop, AbuFactorPreAtrNSto
 from abupy import abu, ABuProgress, AbuMetricsBase, EMarketTargetType, nd
 
 from learn_python.ABuFactorHaiGuiBuyBreak import ABuFactorHaiGuiBuyBreak
+from learn_python.ABuFactorBuyDMNew import AbuDoubleMaBuyNew
 
 warnings.filterwarnings('ignore')
 sns.set_context(rc={'figure.figsize': (14, 7)})
@@ -32,27 +33,28 @@ abupy.env.disable_example_env_ipython()
 
 
 def get_data():
-    futu_data = ABuSymbolPd.make_kl_df('usTSLA')
+    futu_data = ABuSymbolPd.make_kl_df('usFUTU')
     print(futu_data.tail())
 
 
 def execute_test(show=True):
     # 初始资金量
-    cash = 3000000
+    cash = 1000000
 
     # 使用沙盒内的美股做为回测目标
-    us_choice_symbols = ['usFUTU']
+    us_choice_symbols = ['usTSLA']
 
-    # us_choice_symbols = ['usTSLA', 'usNOAH', 'usSFUN', 'usBIDU', 'usAAPL',
+    # us_choice_symbols = ['usFUTU', 'usTSLA', 'usNOAH', 'usSFUN', 'usBIDU', 'usAAPL',
     #                      'usGOOG', 'usWUBA', 'usVIPS']
 
 
 
 
     # 买入双均线策略AbuDoubleMaBuy寻找金叉买入信号：ma快线＝5，ma慢线＝60
-    buy_factors = [{'fast': 5, 'slow': 60, 'class': AbuDoubleMaBuy}]
+    buy_factors = [{'fast': 5, 'slow': 30, 'class': AbuDoubleMaBuyNew}]
     # 卖出双均线策略AbuDoubleMaSell寻找死叉卖出信号：ma快线＝5，ma慢线＝60，并行继续使用止盈止损基础策略
-    sell_factors = [{'fast': 5, 'slow': 60, 'class': AbuDoubleMaSell},
+    sell_factors = [
+                    {'fast': 5, 'slow': 10, 'class': AbuDoubleMaSell},
                     {'stop_loss_n': 1.0, 'stop_win_n': 3.0,
                      'class': AbuFactorAtrNStop},
                     {'class': AbuFactorPreAtrNStop, 'pre_atr_n': 1.5},
@@ -83,18 +85,23 @@ def execute_test(show=True):
 
     metrics.plot_buy_factors()
     metrics.plot_sell_factors()
+    metrics.plot_order_returns_cmp()
+    metrics.plot_sharp_volatility_cmp()
 
-    tsla_orders = abu_result_tuple.orders_pd[abu_result_tuple.orders_pd.symbol == 'usFUTU']
+    tsla_orders = abu_result_tuple.orders_pd[abu_result_tuple.orders_pd.symbol == 'usTSLA']
 
     print(tsla_orders)
 
     nd.ma.plot_ma_from_order(tsla_orders.iloc[0], time_period=(5, 60))
     nd.ma.plot_ma_from_order(tsla_orders.iloc[1], time_period=(5, 60))
+    nd.ma.plot_ma_from_order(tsla_orders.iloc[2], time_period=(5, 60))
+    nd.ma.plot_ma_from_order(tsla_orders.iloc[3], time_period=(5, 60))
+    nd.ma.plot_ma_from_order(tsla_orders.iloc[4], time_period=(5, 60))
 
 
 def execute_test_new(show=True):
     # 初始资金量
-    cash = 3000000
+    cash = 1000000
 
     # 使用沙盒内的美股做为回测目标
     us_choice_symbols = ['usFUTU']
@@ -148,12 +155,12 @@ def execute_test_new(show=True):
 
 def execute_test_new_1(show=True):
     # 初始资金量
-    cash = 3000000
+    cash = 1000000
 
     # 使用沙盒内的美股做为回测目标
-    us_choice_symbols = ['usFUTU']
+    us_choice_symbols = ['usTSLA']
 
-    # us_choice_symbols = ['usTSLA', 'usNOAH', 'usSFUN', 'usBIDU', 'usAAPL',
+    # us_choice_symbols = ['usFUTU', 'usTSLA', 'usNOAH', 'usSFUN', 'usBIDU', 'usAAPL',
     #                      'usGOOG', 'usWUBA', 'usVIPS']
 
     kl_dict = {us_symbol[2:]:
@@ -195,8 +202,10 @@ def execute_test_new_1(show=True):
 
     metrics.plot_buy_factors()
     metrics.plot_sell_factors()
+    metrics.plot_order_returns_cmp()
+    metrics.plot_sharp_volatility_cmp()
 
-    tsla_orders = abu_result_tuple.orders_pd[abu_result_tuple.orders_pd.symbol == 'usFUTU']
+    tsla_orders = abu_result_tuple.orders_pd[abu_result_tuple.orders_pd.symbol == 'usTSLA']
 
     print(tsla_orders)
 
