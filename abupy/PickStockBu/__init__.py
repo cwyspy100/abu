2fd47bdbfab03c09eb48539d9a22e3b2694bf27f
cwyspy100 <cwyspy100@163.com>
2024-04-10 18:12:52 +0800

    test pick stock by mean

--- abupy/PickStockBu/__init__.py
+++ abupy/PickStockBu/__init__.py
@@ -6,12 +6,15 @@ from .ABuPickStockBase import AbuPickStockBase
 from .ABuPickStockPriceMinMax import AbuPickStockPriceMinMax
 from .ABuPickStockDemo import AbuPickStockShiftDistance, AbuPickStockNTop
 from . import ABuPickStock as ps
+from .ABuPickStockByMean import AbuPickStockByMean
 
 __all__ = [
     'AbuPickRegressAngMinMax',
+    'AbuPickSimilarNTop',
     'AbuPickSimilarNTop',
     'AbuPickStockBase',
     'AbuPickStockPriceMinMax',
     'AbuPickStockShiftDistance',
     'AbuPickStockNTop',
+    'AbuPickStockByMean',
     'ps']
